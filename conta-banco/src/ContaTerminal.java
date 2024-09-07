f21a1d3abeb105763aae210638252e699173e206
Pricilla de Souza Teixeira <Pricilla.souza2@gmail.com>
2024-09-07 15:29:22 -0300

    segundo commit

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,15 +1,34 @@
 import java.util.Scanner;
 
 public class ContaTerminal {
+  
     public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
 
-        int numero;
+        int numeroConta;
         String agencia;
         String nomeCliente;
         double saldo;
-        
-        Scanner scan = new Scanner(System.in);
 
+        System.out.println("Digite o seu nome");
+        nomeCliente = scan.nextLine();
+
+        System.out.println("Digite o numero da sua conta");
+        numeroConta = scan.nextInt();
+
+        System.out.println("Digite o numero da sua agencia");
+        agencia = scan.nextLine();
+
+        System.out.println("Digite o seu saldo");
+        saldo = scan.nextDouble();
+
+   
+        System.out.println("OBTEMOS OS SEGUINTES DADOS: ");
+        System.out.println("nome da Cliente: "+nomeCliente);
+        System.out.println("numero da conta: "+numeroConta);
+        System.out.println("numero da agência: "+agencia);
+        System.out.println("Saldo da conta "+saldo);
+   
 
     }
 }
